d9ac035a245287812b062e4e03fe00579e265193
Garth天卜 <g-art-h@outlook.com>
2025-04-05 03:30:16 +0800

    优化编码连接器

--- src/route_connector.rs
+++ src/route_connector.rs
@@ -1,22 +1,22 @@
 use std::collections::HashMap;
 
 pub(crate) struct RouteConnector {
-    /// 按键之间的用时当量，键为两个按键，值为用时
-    time_cost_map: HashMap<String, f64>,
+    /// 按键之间的用时当量，键为两个按键，值为用时当量
+    time_map: HashMap<String, f64>,
     /// 连接方法代号：0-空格或符号，1-无间隔，2-键道顶功
     method_code: usize,
 }
 
 impl RouteConnector {
-    fn new(time_cost_map: HashMap<String, f64>, method_code: usize) -> RouteConnector {
+    fn new(time_map: HashMap<String, f64>, method_code: usize) -> RouteConnector {
         RouteConnector {
-            time_cost_map,
+            time_map,
             method_code,
         }
     }
 
-    fn get_time_cost(&self, text: &str) -> f64 {
-        let mut total = 0.0;
+    pub(crate) fn get_time(&self, text: &str) -> f64 {
+        let mut sum = 0.0;
         let mut chars = text.chars().peekable();
         let mut key = String::with_capacity(2);
 
@@ -25,116 +25,120 @@ impl RouteConnector {
                 key.clear();
                 key.push(c1);
                 key.push(c2);
-                match self.time_cost_map.get(&key) {
-                    Some(value) => total += value,
+
+                match self.time_map.get(&key) {
+                    Some(value) => sum += value,
                     None => {
                         println!("找不到{key}对应的当量。已默认为1.4。");
-                        total += 1.4;
+                        sum += 1.4;
                     }
                 }
             }
         }
 
-        total
+        sum
     }
 
-    pub(crate) fn connect(
-        &self,
-        head_code: &str,
-        head_time_cost: f64,
-        tail_code: &str,
-        tail_time_cost: f64,
-    ) -> (String, f64) {
-        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        let numbers = "0123456789";
+    pub(crate) fn connect(&self, s1: &str, t1: f64, s2: &str, t2: f64) -> (String, f64) {
+        let a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let n = "0123456789";
 
-        let connect_with = |mid_chars: &str, head_last: char, tail_first: char| {
-            let mut route = String::new();
-            route.push_str(head_code);
-            route.push_str(mid_chars);
-            route.push_str(tail_code);
-            let mut mid_str = String::new();
-            mid_str.push(head_last);
-            mid_str.push_str(mid_chars);
-            mid_str.push(tail_first);
-            let time_cost = head_time_cost + tail_time_cost + self.get_time_cost(&mid_str);
-            (route, time_cost)
+        // t1 == 0.0 表示没有上文
+        let s1_last = if t1 > 0.0 {
+            s1.chars().next_back().expect("无法获取前部末字符。")
+        } else {
+            '\0'
+        };
+        let s2_first = s2.chars().next().expect("无法获取后部首字符。");
+        let s2_last = s2.chars().next_back().expect("无法获取后部末字符。");
+
+        let directly = || {
+            let s: String = s1.chars().chain(s2.chars()).collect();
+            let mut m = String::with_capacity(2);
+            m.push(s1_last);
+            m.push(s2_first);
+            let t = t1 + t2 + self.get_time(&m);
+            (s, t)
+        };
+
+        let directly_with_space = || {
+            let mut s = s1.to_string();
+            s.push(' ');
+            s.push_str(s2);
+            let mut m = String::with_capacity(3);
+            m.push(s1_last);
+            m.push(' ');
+            m.push(s2_first);
+            let t = t1 + t2 + self.get_time(&m);
+            (s, t)
         };
 
         match self.method_code {
             0 => {
-                if head_code.is_empty() {
-                    (tail_code.into_string(), tail_time_cost)
+                if t1 == 0.0 {
+                    (s2.to_string(), t2)
+                } else if a.contains(s1_last) && (a.contains(s2_first) || n.contains(s2_first)) {
+                    directly_with_space() // 字母 + (字母或数字)，则加空格
                 } else {
-                    let head_last = head_code.chars().next_back().expect("无法获取头部末字符。");
-                    let tail_first = tail_code.chars().next().expect("无法获取尾部首字符。");
-
-                    // 字母 + (字母或数字)，则加空格
-                    match letters.contains(head_last)
-                        && (letters.contains(tail_first) || numbers.contains(tail_first))
-                    {
-                        true => connect_with(" ", head_last, tail_first),
-                        false => connect_with("", head_last, tail_first),
-                    }
+                    directly()
                 }
             }
             1 => {
-                let head_last = head_code.chars().next_back().expect("无法获取头部末字符。");
-                let tail_first = tail_code.chars().next().expect("无法获取尾部首字符。");
-                connect_with("", head_last, tail_first)
+                if t1 == 0.0 {
+                    (s2.to_string(), t2)
+                } else {
+                    directly()
+                }
             }
             2 => {
-                let xing = "aiouv"; // 形码码元
-                let yin = "bcdefghjklmnpqrstwxyz"; // 音码码元
-                let mut head_chars: Vec<char> = head_code.chars().collect();
-                let mut tail_chars: Vec<char> = tail_code.chars().collect();
-                let mut real_head_time_cost = head_time_cost;
-                let mut real_tail_time_cost = tail_time_cost;
+                let x = "aiouv"; // 形码码元
+                let y = "bcdefghjklmnpqrstwxyz"; // 音码码元
 
-                // 新码不足4码，且以音码结尾：后补空格
-                if tail_chars.len() < 4 && yin.contains(tail_chars[tail_chars.len() - 1]) {
-                    tail_chars.push(' ');
-                    let new_part = tail_chars[tail_chars.len() - 2..]
-                        .iter()
-                        .collect::<String>();
-                    real_tail_time_cost += self.get_time_cost(&new_part);
+                let mut s2_chars: Vec<char> = s2.chars().collect();
+                let mut mod_t2 = t2;
+
+                // 新码以音码结尾，且不足4码：后补空格
+                if y.contains(s2_last) && s2_chars.len() < 4 {
+                    s2_chars.push(' ');
+                    let new_part: String = s2_chars[s2_chars.len() - 2..].iter().collect();
+                    mod_t2 += self.get_time(&new_part);
                 }
                 // 没有上文：直接返回
-                if head_chars.len() == 0 {
-                    return (tail_chars.iter().collect(), real_tail_time_cost);
+                if t1 == 0.0 {
+                    return (s2_chars.iter().collect(), mod_t2);
                 }
 
-                // 新码以非空格的标点开头，且前为空格：去掉空格
-                if head_chars[head_chars.len() - 1] == ' '
-                    && tail_chars[0] != ' '
-                    && !letters.contains(tail_chars[0])
-                    && !numbers.contains(tail_chars[0])
+                let mut s1_chars: Vec<char> = s1.chars().collect();
+                let mut mod_t1 = t1;
+
+                // 新码以非空格的标点开头，且上文末尾为空格：去掉上文末尾的空格
+                if s1_last == ' '
+                    && s2_first != ' '
+                    && !a.contains(s2_first)
+                    && !n.contains(s2_first)
                 {
-                    let removed_part = head_chars[head_chars.len() - 2..]
-                        .iter()
-                        .collect::<String>();
-                    real_head_time_cost -= self.get_time_cost(&removed_part);
-                    head_chars.pop().expect("无法删除头部末字符。");
+                    if s1_chars.len() > 1 {
+                        let removed_part: String = s1_chars[s1_chars.len() - 2..].iter().collect();
+                        mod_t1 -= self.get_time(&removed_part);
+                    }
+                    s1_chars.pop().expect("无法删除头部末尾的空格。");
                 }
-                // 新码以形码或数字开头，且前为字母：前加空格
-                else if letters.contains(head_chars[head_chars.len() - 1])
-                    && (xing.contains(tail_chars[0]) || numbers.contains(tail_chars[0]))
-                {
-                    head_chars.push(' ');
-                    let new_part = tail_chars[tail_chars.len() - 2..]
-                        .iter()
-                        .collect::<String>();
-                    real_head_time_cost += self.get_time_cost(&new_part);
+                // 新码以形码或数字开头，且上文末尾为字母：在上文末尾加空格
+                else if a.contains(s1_last) && (x.contains(s2_first) || n.contains(s2_first)) {
+                    s1_chars.push(' ');
+                    if s1_chars.len() > 1 {
+                        let new_part: String = s1_chars[s1_chars.len() - 2..].iter().collect();
+                        mod_t1 += self.get_time(&new_part);
+                    }
                 }
 
                 // 连接
-                head_chars.append(&mut tail_chars);
-                let mut route: String = head_chars.iter().collect();
+                s1_chars.append(&mut s2_chars);
+                let route: String = s1_chars.iter().collect();
                 let mut mid_str = String::new();
-                mid_str.push(head_chars[head_chars.len() - 1]);
-                mid_str.push(tail_chars[0]);
-                let time_cost =
-                    real_head_time_cost + real_tail_time_cost + self.get_time_cost(&mid_str);
+                mid_str.push(s1_chars[s1_chars.len() - 1]);
+                mid_str.push(s2_chars[0]);
+                let time_cost = mod_t1 + mod_t2 + self.get_time(&mid_str);
                 (route, time_cost)
             }
             _ => panic!("未知的连接方法代号。"),

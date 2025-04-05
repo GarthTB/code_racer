8def0f3b18f773cbeb4c2590826321b078b50fcf
Garth天卜 <g-art-h@outlook.com>
2025-04-06 02:03:19 +0800

    补充读取用户输入的代码

--- src/console_reader.rs
+++ src/console_reader.rs
@@ -1,45 +1,61 @@
+use crate::dict_loader;
+use crate::route_connector::RouteConnector;
+use std::collections::HashMap;
 use std::path::PathBuf;
 
-fn read() -> String {
+pub(crate) fn read() -> String {
     let mut input = String::new();
     loop {
-        if let Ok(_) = std::io::stdin().read_line(&mut input) {
-            return input.trim().to_string();
+        match std::io::stdin().read_line(&mut input) {
+            Ok(_) => return input.trim().to_string(),
+            Err(_) => println!("无法读取输入。请重新输入。"),
         }
-        println!("无法读取输入。请重新输入。");
     }
 }
 
-fn get_method_code() -> usize {
-    println!("请输入连接方法代号：0-空格或符号，1-无间隔，2-键道顶功");
+pub(crate) fn get_connector(time_map: HashMap<String, f64>) -> RouteConnector {
+    println!("请输入连接方法代号：");
+    println!("0: 空格或符号; 1: 无间隔; 2: 键道顶功");
     loop {
         if let Ok(code) = read().parse() {
             if code < 3 {
-                return code;
+                return RouteConnector::new(time_map, code);
             }
         }
-        println!("无效代号。请重新输入。");
+        println!("无效代号。请重新输入。")
     }
 }
 
-fn get_dict_path() -> PathBuf {
+pub(crate) fn get_dict(
+    punct_items: Vec<(String, String, usize)>,
+    connector: &RouteConnector,
+) -> HashMap<char, HashMap<String, (String, f64)>> {
     println!("请输入词库文件路径：");
     loop {
         let path = PathBuf::from(read());
-        if path.exists() {
-            return path;
+        match path.exists() {
+            true => match dict_loader::load_dict(&path, punct_items.clone(), connector) {
+                Ok(dict) => return dict,
+                Err(e) => println!("{}。请重新输入。", e),
+            },
+            false => println!("文件不存在。请重新输入。"),
         }
-        println!("文件不存在。请重新输入。");
     }
 }
 
-fn get_text_path() -> PathBuf {
+pub(crate) fn get_text() -> Vec<char> {
     println!("请输入待编码文本文件路径：");
     loop {
         let path = PathBuf::from(read());
-        if path.exists() {
-            return path;
+        match path.exists() {
+            true => match std::fs::read_to_string(path) {
+                Ok(text) => match text.is_empty() {
+                    false => return text.chars().collect(),
+                    true => println!("文件为空。请重新输入。"),
+                },
+                Err(_) => println!("无法读取文件。请重新输入。"),
+            },
+            false => println!("文件不存在。请重新输入。"),
         }
-        println!("文件不存在。请重新输入。");
     }
 }

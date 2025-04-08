ce07e35dd6307f1e5a1ef2c7bed6541fbc80f14e
Garth天卜 <g-art-h@outlook.com>
2025-04-09 01:29:52 +0800

    改进编码性能

--- src/route_connector.rs
+++ src/route_connector.rs
@@ -40,7 +40,7 @@ impl RouteConnector {
         crate::report_saver::save(text_path, "找不到当量的按键组合", content);
     }
 
-    pub(crate) fn get_time(&mut self, chars: &Vec<char>) -> f64 {
+    pub(crate) fn get_time(&mut self, chars: &[char]) -> f64 {
         let mut sum = 0.0;
         for i in 0..chars.len() - 1 {
             let key = (chars[i], chars[i + 1]);
@@ -52,24 +52,20 @@ impl RouteConnector {
                 }
             }
         }
-        (sum * 1000.0).round() / 1000.0 // 避免浮点数精度问题
+        (sum * 100.0).round() / 100.0 // 避免浮点数精度问题
     }
 
-    pub(crate) fn connect(&mut self, s1: &str, s2: &str, t1: f64, t2: f64) -> (String, f64) {
-        // 取出前部的末字符、后部首字符、后部的末字符
-        let s1_last = if s1.is_empty() {
-            '\0'
-        } else {
-            s1.chars().next_back().expect("无法获取前部末字符")
-        };
-        let (s2_first, s2_last) = if s2.is_empty() {
-            ('\0', '\0')
-        } else {
-            (
-                s2.chars().next().expect("无法获取后部首字符"),
-                s2.chars().next_back().expect("无法获取后部末字符"),
-            )
-        };
+    pub(crate) fn connect(
+        &mut self,
+        s1: &[char],
+        s2: &[char],
+        t1: f64,
+        t2: f64,
+    ) -> (Vec<char>, f64) {
+        // 取出前部的末字符、后部的首字符、后部的末字符
+        let s1_last = s1.last().unwrap_or(&'\0');
+        let s2_first = s2.first().unwrap_or(&'\0');
+        let s2_last = s2.last().unwrap_or(&'\0');
 
         fn is_letter(c: char) -> bool {
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".contains(c)
@@ -93,73 +89,75 @@ impl RouteConnector {
             0 => {
                 // 字母 + (字母或数字)，则加空格
                 if s1.is_empty() {
-                    (s2.to_string(), t2)
-                } else if is_letter(s1_last) && (is_letter(s2_first) || is_number(s2_first)) {
-                    let mut s = s1.to_string();
-                    s.push(' ');
-                    s.push_str(s2);
-                    let mid = vec![s1_last, ' ', s2_first];
+                    (s2.to_vec(), t2)
+                } else if is_letter(*s1_last) && (is_letter(*s2_first) || is_number(*s2_first)) {
+                    let mid = vec![*s1_last, ' ', *s2_first];
                     let t = t1 + t2 + self.get_time(&mid);
+                    let mut s = s1.to_vec();
+                    s.push(' ');
+                    s.append(&mut s2.to_vec());
                     (s, t)
                 } else {
-                    let s: String = s1.chars().chain(s2.chars()).collect();
-                    let mid = vec![s1_last, s2_first];
+                    let mid = vec![*s1_last, *s2_first];
                     let t = t1 + t2 + self.get_time(&mid);
+                    let mut s = s1.to_vec();
+                    s.append(&mut s2.to_vec());
                     (s, t)
                 }
             }
             1 => {
                 if s1.is_empty() {
-                    (s2.to_string(), t2)
+                    (s2.to_vec(), t2)
                 } else {
-                    let s: String = s1.chars().chain(s2.chars()).collect();
-                    let mid = vec![s1_last, s2_first];
+                    let mid = vec![*s1_last, *s2_first];
                     let t = t1 + t2 + self.get_time(&mid);
+                    let mut s = s1.to_vec();
+                    s.append(&mut s2.to_vec());
                     (s, t)
                 }
             }
             2 => {
-                let mut s2_chars: Vec<char> = s2.chars().collect();
+                let mut mod_s2 = s2.to_vec();
                 let mut mod_t2 = t2;
 
                 // 新码以音码结尾，且不足4码：新码后补空格
-                if is_yin(s2_last) && s2_chars.len() < 4 {
-                    s2_chars.push(' ');
-                    let tail = vec![s2_last, ' '];
-                    mod_t2 += self.get_time(&tail);
+                if is_yin(*s2_last) && s2.len() < 4 {
+                    mod_s2.push(' ');
+                    mod_t2 += self.get_time(&mod_s2[mod_s2.len() - 2..]);
                 }
                 // 没有上文：直接返回
                 if s1.is_empty() {
-                    return (s2_chars.into_iter().collect(), mod_t2);
+                    return (s2.to_vec(), mod_t2);
                 }
 
-                let mut s1_chars: Vec<char> = s1.chars().collect();
+                let mut mod_s1 = s1.to_vec();
                 let mut mod_t1 = t1;
 
-                // 上文末尾为字母后的空格，且新码以非空格的标点开头：去掉上文末尾的空格
-                if s1_chars.len() > 1
-                    && is_letter(s1_chars[s1_chars.len() - 2])
-                    && s1_last == ' '
-                    && s2_first != ' '
-                    && !is_letter(s2_first)
-                    && !is_number(s2_first)
+                // 上文末尾为音码 + 空格，且新码以非空格的标点开头：去掉上文末尾的空格
+                if s1.len() > 1
+                    && is_yin(s1[s1.len() - 2])
+                    && *s1_last == ' '
+                    && *s2_first != ' '
+                    && !is_letter(*s2_first)
+                    && !is_number(*s2_first)
                 {
-                    s1_chars.pop().expect("无法删除头部末尾的空格");
-                    let tail = vec![s1_chars[s1_chars.len() - 1], ' '];
-                    mod_t1 -= self.get_time(&tail);
+                    mod_s1.pop().expect("无法删除头部末尾的空格");
+                    mod_t1 -= self.get_time(&s1[s1.len() - 2..]);
                 }
                 // 上文末尾为字母，且新码以形码或数字开头：在上文末尾加空格
-                else if is_letter(s1_last) && (is_xing(s2_first) || is_number(s2_first)) {
-                    s1_chars.push(' ');
-                    let tail = vec![s1_last, ' '];
-                    mod_t1 += self.get_time(&tail);
+                else if is_letter(*s1_last) && (is_xing(*s2_first) || is_number(*s2_first)) {
+                    mod_s1.push(' ');
+                    mod_t1 += self.get_time(&mod_s1[mod_s1.len() - 2..]);
                 }
 
                 // 连接
-                let mid = vec![s1_chars[s1_chars.len() - 1], s2_chars[0]];
+                let mid = vec![
+                    *mod_s1.last().expect("无法获取头部末字符"),
+                    *mod_s2.first().expect("无法获取尾部首字符"),
+                ];
                 let time = mod_t1 + mod_t2 + self.get_time(&mid);
-                s1_chars.append(&mut s2_chars);
-                (s1_chars.into_iter().collect(), time)
+                mod_s1.append(&mut mod_s2);
+                (mod_s1, time)
             }
             _ => panic!("未知的连接方法代号"),
         }
